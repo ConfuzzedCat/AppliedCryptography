1a933f64f9fcbfb22c61a9e06c47b46be83b4406
ConfuzzedCat <confuzzedcat@gmail.com>
2026-04-17 14:24:18 +0200

    solution1 done using fernet and a key file.

--- client.py
+++ client.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 
 from socket import *
+
+from Cython.Compiler.Naming import enc_scope_cname
+from cryptography.fernet import Fernet
+
+
 server_name = input("Enter your server's ip: ")
 server_port = int(input("Enter your server's port: "))
+enc_key = ""
+try:
+    with open("key.txt") as f:
+        enc_key = f.read().encode()
+except FileNotFoundError:
+    enc_key = input("Enter your encryption key: ")
 client_socket = socket(AF_INET,SOCK_STREAM)
 client_socket.connect((server_name,server_port))
-
+fer = Fernet(enc_key)
 bCloseConnection = False
 
 try:
     while not bCloseConnection:
         message = input("Please enter your message: ")
-        client_socket.send(message.encode())
-        modified_message = client_socket.recv(2048)
-        print("From server: ", modified_message.decode())
+        client_socket.send(fer.encrypt(message.encode()))
+        server_message = fer.decrypt(client_socket.recv(2048)).decode()
+
+        print("From server: ", server_message)
         if message == "bye":
             bCloseConnection = True
 except KeyboardInterrupt:
--- server.py
+++ server.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 
 from socket import *
+from cryptography.fernet import Fernet
 
 IP = ''
 PORT = 12001
+
+KEY = ''
+KEY_FILE = 'key.txt'
+
+# Get or generate key
+bSaveKey = False
+with (open(KEY_FILE) as f):
+    key_temp = f.readline().strip()
+    if key_temp == "":
+        bSaveKey = True
+        key_temp = Fernet.generate_key()
+    KEY = key_temp
+
+if bSaveKey:
+    with open(KEY_FILE, "w") as f:
+        f.write(KEY.decode("utf-8"))
+
 server_port = PORT
 server_socket = socket(AF_INET,SOCK_STREAM)
 server_socket.bind(('',server_port))
 server_socket.listen(1)
+fer = Fernet(KEY)
 print(f"The server is ready to receive ({server_socket.getsockname()})")
 try:
     while True:
@@ -15,8 +34,8 @@ try:
         message = ""
         bCloseConnection = False
         while not bCloseConnection:
-            message = conn_socket.recv(2048).decode()
-            conn_socket.send(message.encode())
+            message = fer.decrypt(conn_socket.recv(2048)).decode()
+            conn_socket.send(fer.encrypt(message.encode()))
             print("connection received from {}, and {} is sent back".format(client_address[1], message))
             if message == "bye":
                 bCloseConnection = True
